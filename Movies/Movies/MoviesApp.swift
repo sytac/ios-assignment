1fafdd99470e752f674065250f6e3ad21bf20c44
xdmarcos <xdmarcos@hotmail.com>
2021-04-12 15:28:20 +0100

    Add Xcode project

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by xdmgzdev on 12/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
